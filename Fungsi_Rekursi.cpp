fa007d572cb82fd225ce2547ea955931644c4744
djvahd <adrielharjanto@gmail.com>
2025-09-27 00:38:05 +0700

    Latihan

---
+++ Fungsi_Rekursi.cpp
@@ -0,0 +1,27 @@
+/*
+Nama Program    : Mengubah Desimal ke Biner
+Nama            : Jovanic Adriel Harjanto
+NPM             : 140810250050
+Tanggal Buat    : 22 September 2025
+Deskripsi       : Mengubah desimal ke biner dengan fungsi rekursi
+*/
+
+#include <iostream>
+using namespace std;
+
+void desimalKeBiner(int n) {
+    if (n > 1) desimalKeBiner(n / 2);
+    cout << n % 2;
+}
+
+int main() {
+    int angka;
+    cout << "Masukkan angka desimal: ";
+    cin >> angka;
+
+    cout << "Bentuk biner: ";
+    desimalKeBiner(angka);
+    cout << endl;
+
+    return 0;
+}
